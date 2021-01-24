425feb2bca78be6204676a537c343ca97f652cba
Tungky Adi Pradana <adi.pradana95@gmail.com>
2021-01-24 21:20:57 +0700

    added some code in Soal2.go

--- Soal_2/Soal2.go
+++ Soal_2/Soal2.go
@@ -3,18 +3,36 @@ package main
 import (
     "encoding/json"
     "fmt"
-    "io"
-    "log"
-    "strings"
 )
 
 type profileList struct {
-    id int
-    username string
-    profile []string
-
+    id int `json:"id"`
+    username string `json:"username"`
+    profile profile `json:"profile"`
+    // articles []articles `json:articles`
 }
 
-func main() {
+type profile struct {
+    full_name string `json:"full_name"`
+    birthday string `json:"birthday"`
+    phones string `json:phones`
+}
 
+// type articles []struct {
+//
+// }
+
+func main() {
+    fmt.Println("Test json manipulation")
+    profile := profile{full_name: "Adi Pradana", birthday: "1995-12-21", phones: "021"}
+    profileList := profileList{id: 123, username: "admin", profile: profile}
+
+    fmt.Printf("%+v\n",profileList)
+    var jsonData []byte
+    jsonData, err := json.MarshalIndent(profileList, "", "  ")
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    fmt.Println(string(jsonData))
 }
