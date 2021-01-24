84ee59ae8e61b726638debc63ae514eba7aa2b7a
Tungky Adi Pradana <adi.pradana95@gmail.com>
2021-01-24 14:39:58 +0700

    Add some code and json file for complete the skill test

---
+++ Soal_2/Soal2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "io"
+    "log"
+    "strings"
+)
+
+type profileList struct {
+    id int
+    username string
+    profile []string
+
+}
+
+func main() {
+
+}
